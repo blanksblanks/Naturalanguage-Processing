37d2d9ae0d1dc00d93081923245afde059485a8a
Nina Baculinao <nb2406@columbia.edu>
2015-02-25 04:59:15 -0500

    feat(transition.py): implement incredibly low performance transitions -UAS: 0.140211113324 -LAS: 0.0543716391157

--- Homework2/transition.py
+++ Homework2/transition.py
@@ -18,8 +18,25 @@ class Transition(object):
             :param configuration: is the current configuration
             :return : A new configuration or -1 if the pre-condition is not satisfied
         """
-        raise NotImplementedError('Please implement left_arc!')
-        return -1
+        # raise NotImplementedError('Please implement left_arc!')
+        # return -1
+
+        if not conf.buffer or not conf.stack:
+            return -1
+
+        # add the arc (b, L, s) to set of arcs A and pop stack
+        # that is, draw arc bt next node on the buffer and on stack with label L
+        idx_wi = conf.stack[-1] # s
+        idx_wj = conf.buffer.pop(0) # b
+        
+        if idx_wi is 0 or (idx_wj, relation, idx_wi) in conf.arcs:
+            return -1
+
+        print 'left arc'
+        print (idx_wj, relation, idx_wi)
+
+        conf.stack = conf.stack[:-1] # pop stack / remove last element
+        conf.arcs.append((idx_wj, relation, idx_wi)) # add the arc (b, L, s) to A
 
     @staticmethod
     def right_arc(conf, relation):
@@ -32,10 +49,18 @@ class Transition(object):
 
         # You get this one for free! Use it as an example.
 
+        # add the arc (s, L, b) to A, and push b onto stack where
+        # s:idx_wi is the next node on the stack and b:idx_wj is 
+        # the next node on the buffer
         idx_wi = conf.stack[-1]
         idx_wj = conf.buffer.pop(0)
 
-        conf.stack.append(idx_wj)
+        if idx_wi is 0 or (idx_wj, relation, idx_wi) in conf.arcs:
+            return -1
+
+        print 'right arc'
+        print (idx_wi, relation, idx_wj)
+        conf.stack.append(idx_wj) # push b onto stack
         conf.arcs.append((idx_wi, relation, idx_wj))
 
     @staticmethod
@@ -44,8 +69,15 @@ class Transition(object):
             :param configuration: is the current configuration
             :return : A new configuration or -1 if the pre-condition is not satisfied
         """
-        raise NotImplementedError('Please implement reduce!')
-        return -1
+        # raise NotImplementedError('Please implement reduce!')
+        if not conf.buffer or not conf.stack:
+            return -1
+
+        print 'reduce'
+        # remove b from buffer and add to stack
+        idx_wj = conf.buffer.pop(0)
+        conf.stack.append(idx_wj)
+
 
     @staticmethod
     def shift(conf):
@@ -53,5 +85,14 @@ class Transition(object):
             :param configuration: is the current configuration
             :return : A new configuration or -1 if the pre-condition is not satisfied
         """
-        raise NotImplementedError('Please implement shift!')
-        return -1
+        # raise NotImplementedError('Please implement shift!')
+        if not conf.stack:
+            return -1
+
+        # check preconditions, s cannot be root
+        if conf.stack[-1] is 0:
+            return -1
+        print 'shift'
+        # pop sigma
+        conf.stack = conf.stack[:-1]
+
