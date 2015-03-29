550091e0476760eb71413b1407b64c2cbed49449
Nina Baculinao <nb2406@columbia.edu>
2015-03-29 11:39:00 -0400

    feat(main.py): add senseid/target data

--- Homework3/main.py
+++ Homework3/main.py
@@ -5,6 +5,9 @@ import sys
 import unicodedata
 from nltk.corpus import wordnet as wn
 import nltk
+from sklearn import datasets
+from sklearn import svm
+from sklearn import neighbors
 
 # 2
 def parse_data(input_file):
@@ -102,23 +105,26 @@ def compute_context_vectors(language):
 	xmldoc = minidom.parse(input_file)
 	s_i_data = {}
         s_data = {}
-        context_vex = {}
+        ans_data = {}
+        context_data = []
         k = 10
 	lex_list = xmldoc.getElementsByTagName('lexelt')
 	for node in lex_list:
 		lexelt = node.getAttribute('item')
 		print 'lexelt', lexelt
                 s_i_data[lexelt] = []
+                ans_data[lexelt] = []
                 s = []
                 inst_list = node.getElementsByTagName('instance')
 		for inst in inst_list:
 			instance_id = inst.getAttribute('id')
-			l = inst.getElementsByTagName('context')[0]
-			context = (l.childNodes[0].nodeValue + l.childNodes[1].firstChild.nodeValue + l.childNodes[2].nodeValue).replace('\n', '')
+                        l = inst.getElementsByTagName('context')[0]
+			# context = (l.childNodes[0].nodeValue + l.childNodes[1].firstChild.nodeValue + l.childNodes[2].nodeValue).replace('\n', '')
                         pretokens = nltk.word_tokenize(l.childNodes[0].nodeValue)
                         posttokens = nltk.word_tokenize(l.childNodes[2].nodeValue)
                         tokens = pretokens[-k:]
                         tokens.extend(posttokens[:k])
+                        print instance_id, 'tokens', tokens
                         s.extend(tokens)
                         # convert list to dictionary
                         s_i = {}
@@ -128,7 +134,7 @@ def compute_context_vectors(language):
                             else:
                                 s_i[token] = 1
                         # s_i = {item : index for index, item in enumerate(s_i)}
-                        print 's_i', s_i
+                        # print 's_i', s_i
                         # print '0', l.childNodes[0].nodeValue
 			# print '1', l.childNodes[1].firstChild.nodeValue
 			# print '2', l.childNodes[2].nodeValue
@@ -136,12 +142,15 @@ def compute_context_vectors(language):
                         # data[lexelt].append((instance_id, context))
                         s_i_data[lexelt].append((instance_id, s_i))
                         # print 'data[lexelt]',  data[lexelt]
+			# create list of answers
+                        sense_id = inst.getElementsByTagName('answer')[0].getAttribute('senseid')
+			ans_data[lexelt].append((instance_id, sense_id))
                 s = list(set(s))
                 s_data[lexelt] = s
-                print lexelt, 'SET!!!', s_data[lexelt]
+                # print lexelt, 'SET!!!', s_data[lexelt]
                 
                 # calculate context vectors for each instance
-		context_vex[lexelt] = []
+		context_data[lexelt] = []
                 for inst in s_i_data[lexelt]:
                         instance_id = inst[0]
                         s_i = inst[1]
@@ -152,9 +161,11 @@ def compute_context_vectors(language):
                                    vector.append(s_i[word])
                               else:
                                   vector.append(0)
-                        context_vex[lexelt].append((instance_id, vector))
-                        print 'context vector for', instance_id, vector
-	# print 'data', data
+                        context_data[lexelt].append((instance_id, vector))
+                        # print 'context vector for', instance_id, vector
+	return context_data, ans_data
+        # reminder: context_vex[lexelt] = [] -> [(id, vector), (id, vector)]
+        # print 'data', data
 
 
 if __name__ == '__main__':
@@ -163,4 +174,15 @@ if __name__ == '__main__':
 		sys.exit(0)
         # sense_dict = build_dict(sys.argv[1])
 	# most_frequent_sense(sys.argv[1], sense_dict)
-        data = compute_context_vectors(sys.argv[1])
+        vect_data, ans_data  = compute_context_vectors(sys.argv[1])
+        print vect_data
+        print ans_data
+        clf = svm.SVC()
+        list_of_context_vectors = []
+        for lexelt in vect_data:
+                vectors = vect_data[lexelt]
+                list_of_context_vectors.append(vectors[1])
+        for lexelt in ans_data:
+                answers = vect_data[lexelt]
+                list_of_answers.append(answers[1])
+        clf.fit(list_of_context_vectors, list_of_answers)
