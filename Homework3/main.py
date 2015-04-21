9566c3259d9f31bd5ad0d9b0674f9e7ea4ff26a2
Nina Baculinao <nb2406@columbia.edu>
2015-04-21 14:11:56 -0400

    refactor(main.py): move baseline vestigial code to less prominent area

--- Homework3/main.py
+++ Homework3/main.py
@@ -279,6 +279,100 @@ def compute_relevance(s_i_data, lexelt, features):
     # return most_rel, most_pmi
 
 # ============================================================
+# Functions from Baseline.py
+# ============================================================
+
+# 2
+def parse_data(input_file):
+    '''
+    Parse the .xml dev data file
+
+    param str input_file: The input data file path
+    return dict: A dictionary with the following structure
+            {
+                    lexelt: [(instance_id, context), ...],
+                    ...
+            }
+    '''
+    xmldoc = minidom.parse(input_file)
+    data = {}
+    lex_list = xmldoc.getElementsByTagName('lexelt')
+    for node in lex_list:
+        lexelt = node.getAttribute('item')
+        data[lexelt] = []
+        inst_list = node.getElementsByTagName('instance')
+        for inst in inst_list:
+            instance_id = inst.getAttribute('id')
+            if language == 'English':
+                l = inst.getElementsByTagName('context')[0]
+            else:
+                l = inst.getElementsByTagName('target')[0]
+            context = (l.childNodes[0].nodeValue + l.childNodes[1].firstChild.nodeValue + l.childNodes[2].nodeValue).replace('\n', '')
+            # print 'context', context
+            data[lexelt].append((instance_id, context))
+    print 'data', data
+    return data
+
+# 1
+def build_dict(language):
+    '''
+    Count the frequency of each sense
+    '''
+    data = {}
+    xmldoc = minidom.parse('data/' + language + '-train.xml')
+    data = {}
+    '''head_list = xmldoc.getElementsByTagName('head')
+    # print 'head_list', head_list
+    for head in head_list:
+        hd = head.childNodes[0].nodeValue
+            print 'head', hd
+    '''
+    lex_list = xmldoc.getElementsByTagName('lexelt')
+    for node in lex_list:
+        lexelt = node.getAttribute('item')
+        # print 'lexelt', lexelt
+        data[lexelt] = {}
+        inst_list = node.getElementsByTagName('instance')
+        for inst in inst_list:
+            sense_id = inst.getElementsByTagName('answer')[0].getAttribute('senseid')
+            # print 'sense_id',  sense_id
+            try:
+                cnt = data[lexelt][sense_id]
+            except KeyError:
+                data[lexelt][sense_id] = 0
+            data[lexelt][sense_id] += 1
+    
+    record = {}
+    for key, cntDict in data.iteritems():
+        sense = max(cntDict, key = lambda s: cntDict[s])
+        print 'key, cntDict, sense', key, cntDict, sense
+        record[key] = sense
+    print 'record', record
+    return record
+
+# 5
+def getFrequentSense(lexelt, sense_dict):
+    '''
+        Return the most frequent sense of a word (lexelt) in the training set
+        '''
+    sense = ''
+    try:
+        sense = sense_dict[lexelt]
+    except KeyError:
+        pass
+    return sense
+
+
+def most_frequent_sense(language, sense_dict):
+    data = parse_data('data/' + language + '-dev.xml')
+    outfile = codecs.open(language + '.baseline', encoding = 'utf-8', mode = 'w')
+    for lexelt, instances in sorted(data.iteritems(), key = lambda d: replace_accented(d[0].split('.')[0])):
+        for instance_id, context in sorted(instances, key = lambda d: int(d[0].split('.')[-1])):
+            sid = getFrequentSense(lexelt, sense_dict)
+            outfile.write(replace_accented(lexelt + ' ' + instance_id + ' ' + sid + '\n'))
+    outfile.close()
+
+# ============================================================
 # Parsing and Computing Context Vectors
 # ============================================================
 
@@ -293,11 +387,11 @@ If features include 6, compute pointwise mutual information
 If features include 7, compute chi square
 '''
 
-def compute_context_vectors(language, k, features):
+def compute_context_vectors(input_file, language, k, features):
     print 'computing context vectors from training data...'
 
     # define our training data and parse with minidom
-    input_file = 'data/' + language + '-train.xml'
+    # input_file = 'data/' + language + '-train.xml'
     xmldoc = minidom.parse(input_file)
 
     # s_i_data is a dictionary; key=lexelt, value=ordered list of tuples
@@ -439,10 +533,10 @@ def compute_context_vectors(language, k, features):
 
     # reminder: context_vex[lexelt] = [] -> [(id, vector), (id, vector)]
 
-def parse_dev_data(language, k, features, s_data):
+def parse_dev_data(input_file, language, k, features, s_data):
     print 'parsing dev data...'
 
-    input_file = 'data/' + language + '-dev.xml'
+    # input_file = 'data/' + language + '-dev.xml'
     xmldoc = minidom.parse(input_file)
     context_data = {}
     instance_data = {}
@@ -539,12 +633,12 @@ def train_classifiers(context_data, target_data):
 
     return knn_data, svc_data
 
-def test_classifier(language, feats, classifier, clf_data, context_dev, instance_dev):
+def test_classifier(filename, language, feats, classifier, clf_data, context_dev, instance_dev):
     # data = parse_data('data/' + language + '-dev.xml')
-    if (classifier == 'answer'):
-        filename = (language + '.' + classifier)
-    else:
-        filename = (language + '_' + ''.join(feats) + '.' + classifier)
+    # if (classifier == 'answer'):
+    #    filename = (language + '.' + classifier)
+    # else:
+    #    filename = (language + '_' + ''.join(feats) + '.' + classifier)
     print 'writing to', filename + '...'
     outfile = codecs.open(filename, encoding = 'utf-8', mode = 'w')
     for lexelt in context_dev:
@@ -560,114 +654,24 @@ def test_classifier(language, feats, classifier, clf_data, context_dev, instance
     outfile.close()
 
 # ============================================================
-# Functions from Baseline.py
-# ============================================================
-
-# 2
-def parse_data(input_file):
-    '''
-    Parse the .xml dev data file
-
-    param str input_file: The input data file path
-    return dict: A dictionary with the following structure
-            {
-                    lexelt: [(instance_id, context), ...],
-                    ...
-            }
-    '''
-    xmldoc = minidom.parse(input_file)
-    data = {}
-    lex_list = xmldoc.getElementsByTagName('lexelt')
-    for node in lex_list:
-        lexelt = node.getAttribute('item')
-        data[lexelt] = []
-        inst_list = node.getElementsByTagName('instance')
-        for inst in inst_list:
-            instance_id = inst.getAttribute('id')
-            if language == 'English':
-                l = inst.getElementsByTagName('context')[0]
-            else:
-                l = inst.getElementsByTagName('target')[0]
-            context = (l.childNodes[0].nodeValue + l.childNodes[1].firstChild.nodeValue + l.childNodes[2].nodeValue).replace('\n', '')
-            # print 'context', context
-            data[lexelt].append((instance_id, context))
-    print 'data', data
-    return data
-
-# 1
-def build_dict(language):
-    '''
-    Count the frequency of each sense
-    '''
-    data = {}
-    xmldoc = minidom.parse('data/' + language + '-train.xml')
-    data = {}
-    '''head_list = xmldoc.getElementsByTagName('head')
-    # print 'head_list', head_list
-    for head in head_list:
-        hd = head.childNodes[0].nodeValue
-            print 'head', hd
-    '''
-    lex_list = xmldoc.getElementsByTagName('lexelt')
-    for node in lex_list:
-        lexelt = node.getAttribute('item')
-        # print 'lexelt', lexelt
-        data[lexelt] = {}
-        inst_list = node.getElementsByTagName('instance')
-        for inst in inst_list:
-            sense_id = inst.getElementsByTagName('answer')[0].getAttribute('senseid')
-            # print 'sense_id',  sense_id
-            try:
-                cnt = data[lexelt][sense_id]
-            except KeyError:
-                data[lexelt][sense_id] = 0
-            data[lexelt][sense_id] += 1
-    
-    record = {}
-    for key, cntDict in data.iteritems():
-        sense = max(cntDict, key = lambda s: cntDict[s])
-        print 'key, cntDict, sense', key, cntDict, sense
-        record[key] = sense
-    print 'record', record
-    return record
-
-# 5
-def getFrequentSense(lexelt, sense_dict):
-    '''
-        Return the most frequent sense of a word (lexelt) in the training set
-        '''
-    sense = ''
-    try:
-        sense = sense_dict[lexelt]
-    except KeyError:
-        pass
-    return sense
-
-
-def most_frequent_sense(language, sense_dict):
-    data = parse_data('data/' + language + '-dev.xml')
-    outfile = codecs.open(language + '.baseline', encoding = 'utf-8', mode = 'w')
-    for lexelt, instances in sorted(data.iteritems(), key = lambda d: replace_accented(d[0].split('.')[0])):
-        for instance_id, context in sorted(instances, key = lambda d: int(d[0].split('.')[-1])):
-            sid = getFrequentSense(lexelt, sense_dict)
-            outfile.write(replace_accented(lexelt + ' ' + instance_id + ' ' + sid + '\n'))
-    outfile.close()
-
-# ============================================================
 # Main Function Calls
 # ============================================================
 
 if __name__ == '__main__':
     
-    if len(sys.argv) < 2:
-        print 'usage: python baseline.py [language]'
+    if len(sys.argv) != 5:
+        print 'Usage: python main.py <training_file> <test_file> <answer_file> <language>'
+        print 'Example: python main.py data/English-train.xml data/English-dev.xml English.answer English'
         sys.exit(0)
 
-    lang = sys.argv[1]
+    train = sys.argv[1]
+    test = sys.argv[2]
+    ans = sys.argv[3]
+    lang = sys.argv[4]
 
     k, ft = choose_features(lang)
-    context_data, target_data, s_data = compute_context_vectors(lang, k, ft)
+    context_data, target_data, s_data = compute_context_vectors(train, lang, k, ft)
     knn_data, svc_data = train_classifiers(context_data, target_data)
-    context_dev, instance_dev = parse_dev_data(lang, k, ft, s_data)
-    test_classifier(lang, ft, 'answer', svc_data, context_dev, instance_dev)
+    context_dev, instance_dev = parse_dev_data(test, lang, k, ft, s_data)
+    test_classifier(ans, lang, ft, 'answer', svc_data, context_dev, instance_dev)
     
